802e0c5b36ed3bda7f5d3e5bce4d6edeebdc1765
Alan Carden <acarden120@gmail.com>
2015-05-17 16:52:48 +0100

    Complete markdown, rubocop passed

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,7 +1,7 @@
 module WikisHelper
   def markdown_wiki(markdown)
     renderer = Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true}
+    extensions = { fenced_code_blocks: true }
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
