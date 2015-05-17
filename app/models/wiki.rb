95bc06150c3ec0d90d697af0bb897d5c8a7a16cd
il1ja <dalibor.ilijevski@gmail.com>
2015-05-17 22:40:07 +0200

    Added meaningful error message

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -5,8 +5,10 @@ class Wiki < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validate :premium_user_creates_private_wiki, on: :create
 
+  protected
+  
   def premium_user_creates_private_wiki
     return unless wiki_private
-    errors.add(:base, 'nnn') unless user.premium?
+    errors.add(:base, 'Only premium user can create private wiki') unless user.premium?
   end
 end
